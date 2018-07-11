dc49ee31abedf71391a6a2b62abd42ed3bbd755c
Maarten Balliauw <maarten@balliauw.be>
2018-07-11 10:57:21 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -27,11 +27,7 @@ call dotnet restore
 if not "%errorlevel%"=="0" goto failure
 
 REM Build
-REM - Option 1: Run dotnet build for every source folder in the project
-REM   e.g. call dotnet build <path> --configuration %config%
-REM - Option 2: Let msbuild handle things and build the solution
 call "%msbuild%" CamoDotNet.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
-REM call dotnet build --configuration %config%
 if not "%errorlevel%"=="0" goto failure
 
 REM Unit tests
