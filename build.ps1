823fa4db4ae357618de7280e70fbdd8c711581dc
Maarten Balliauw <maarten@maartenballiauw.be>
2020-05-14 09:47:51 +0200

    Update build

--- build.ps1
+++
@@ -1,5 +0,0 @@
-# Install .NET Core
-&powershell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -Channel LTS"
-
-# Run build
-cmd /c run-build.bat
